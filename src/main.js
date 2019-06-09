de19c7c9b5925b939a8b3e93b74a6fd98a1fa113
AlysCole <alyscole@outlook.com>
2019-06-09 19:02:27 +0800

    Redesign page

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import "fotorama/fotorama";
 import "fotorama/fotorama.css";
 
+import "@fortawesome/fontawesome-free/js/all.js";
+import "@fortawesome/fontawesome-free/css/all.css";
+
 import "styles.css";
 
 // Import images:
@@ -15,5 +18,7 @@ $("#angled-image").attr('src', sideview);
 $("#top-image").attr('src', topview);
 
 $(document).ready(function() {
-
+  $(".mobile-menu").click(() => {
+    $("header > .navigation > ul").toggle();
+  });
 });
--- webpack.config.js
+++ webpack.config.js
@@ -65,8 +65,11 @@ module.exports = {
             }
           }
         ]
-
-      }
+      },
+      {
+        "test": /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+        "loader": 'file-loader',
+      },
     ]
   },
 
